1b1c70086b6149964c90083949aa70a5973ac05b
yashtyagi298 <tyagiyash097@gmail.com>
2024-12-02 18:45:58 +0530

    fix-production issue

--- backend/src/index.js
+++ backend/src/index.js
@@ -24,10 +24,10 @@ app.use("/api/auth",authRoutes);
 app.use("/api/message",messageRoute);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+  app.use(express.static(path.join(_dirname, "../../frontend/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../../frontend", "dist", "index.html"));
+    res.sendFile(path.join(_dirname, "../../frontend", "dist", "index.html"));
   });
 }
 
